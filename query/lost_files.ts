7fd90ddc63d8b066bd92280bbb9e4b8a1a3b23e9
Miroslav Sery <athes01@gmail.com>
2026-02-03 19:31:11 +0100

    fix error

--- query/lost_files.ts
+++ query/lost_files.ts
@@ -2,17 +2,17 @@ import fs from 'fs/promises';
 import path from 'path';
 import { mobile_data } from '../mobile_data/mobile_data';
 
-interface PhoneExternal {
+interface UploadedFile {
     media_id: string;  
-    song_lyric_id: number;
+    song_lyric_id: number;  
     song_lyric_name: string; 
 }
 
-const uploaded_files: PhoneExternal[] = [];
+const uploaded_files: UploadedFile[] = [];
 for (const song_lyric of mobile_data.song_lyrics) {
     // Files are stored in the database as "externals".
     for (const external of song_lyric.externals) {
-        if (external.url.startsWith('https://zpevnik.proscholy.cz')) {
+        if (external.url.startsWith('https://zpevnik.proscholy.cz') && external.media_id !== undefined) {
             uploaded_files.push({ 
                 media_id: external.media_id,
                 song_lyric_id: song_lyric.id,
@@ -26,7 +26,7 @@ const dir = path.join(__dirname, '../public_files/zps_public_files');
 const files = await fs.readdir(dir);
 const file_names_set = new Set(files);
 
-const lost_files: Set<PhoneExternal> = new Set();
+const lost_files: Set<UploadedFile> = new Set();
 
 for (const uploaded_file of uploaded_files) {
     if (file_names_set.has(uploaded_file.media_id)) {
